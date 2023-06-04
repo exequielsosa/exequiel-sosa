bb3b0bf6bede856971eea8b16931ff48759e53ce
Exequiel Sosa <exequielsosa@gmail.com>
2023-06-03 22:04:19 -0300

    init flowbite

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import { Button } from 'flowbite-react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -6,6 +7,9 @@ export default function Home() {
   return (
     <main>
      Initial page
+     <div>
+      <Button>Click me</Button>
+    </div>
     </main>
   )
 }
