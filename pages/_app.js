1fb5c535f6aa8533264c9fa987ef6e4f322d3dea
Exequiel Sosa <exequielsosa@gmail.com>
2023-08-05 09:43:26 -0300

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
 import { GralLayout } from "../components/layouts/GralLayout";
 import LayoutMenuAndFooter from "../components/layouts/LayoutMenuAndFooter";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <GralLayout>
       <LayoutMenuAndFooter>
         <Component {...pageProps} />
+        <Analytics />
       </LayoutMenuAndFooter>
     </GralLayout>
   );
